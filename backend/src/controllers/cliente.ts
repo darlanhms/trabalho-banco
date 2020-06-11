1f4b1211f6cbfc6bb1fba15a681078385a2fec8a
Darlan H <darlanhermessjb@hotmail.com>
2020-06-10 22:19:04 -0300

    criar o cliente com endereco;

--- backend/src/controllers/cliente.ts
+++ backend/src/controllers/cliente.ts
@@ -2,12 +2,31 @@ import { Req, Res } from '../types/util';
 import pool from '../connection';
 
 export const createCliente = async (req:Req, res:Res) => {
-    const { nome, email, telefone } = req.body;
+    const { nome, email, telefone, endereco } = req.body;
+    const { logradouro, numero, bairro, cidade, estado } = endereco;
 
-    if (nome && email && telefone) {
+    if (nome && email && telefone && cidade && estado && logradouro) {
         pool.query('INSERT INTO cliente(nome, email, telefone) VALUES ($1::text, $2::text, $3::int) RETURNING *', [nome, email, telefone])
         .then(cliente => {
-            return res.send(cliente.rows);
+            // vaidacao pra checar se recebemos alguma resposta do banco
+            if (cliente.rows && cliente.rows[0] && cliente.rows[0].id) {
+                pool.query(
+                    'INSERT INTO endereco(cliente_id, cidade, estado, logradouro, bairro, numero) VALUES ($1, $2::text, $3::text, $4::text, $5::text, $6::int) RETURNING *', 
+                    [cliente.rows[0].id, cidade, estado, logradouro, bairro, numero]
+                ).then(endereco => {
+                    // vaidacao pra checar se recebemos alguma resposta do banco
+                    if (endereco && endereco.rows[0]) {
+                        cliente.rows[0].endereco = endereco.rows[0];
+                        return res.send(cliente.rows[0]);
+                    } else {
+                        return res.status(500).send("Endereço retornou null");
+                    }
+                }).catch(err => {
+                    return res.status(500).send(err);
+                })
+            } else {
+                return res.status(500).send("Cliente retornou null");
+            }
         }).catch(err => {           
             return res.status(500).send(err);
         })
@@ -17,7 +36,11 @@ export const createCliente = async (req:Req, res:Res) => {
 }
 
 export const findClientes = async (req:Req, res:Res) => {
-    pool.query('SELECT * FROM cliente')
+    pool.query(`
+        SELECT * 
+            FROM cliente c, endereco e 
+            WHERE c.id = e.cliente_id 
+    `)
     .then(clientes => {
         return res.send(clientes.rows);
     }).catch(err => {
--- backend/src/tables.ts
+++ backend/src/tables.ts
@@ -1,10 +1,33 @@
 import pool from './connection';
 
 export const checkTables = async () => {
-    await pool.query(`CREATE TABLE IF NOT EXISTS cliente (
-        id SERIAL PRIMARY KEY NOT NULL, 
-        nome TEXT, 
-        email TEXT, 
-        telefone INT
-    )`)
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS cliente (
+                id SERIAL PRIMARY KEY NOT NULL, 
+                nome VARCHAR(50) NOT NULL, 
+                email VARCHAR(50), 
+                telefone INT
+            )
+        `)
+    } catch (exc) {
+        console.log("Create table cliente exception: ", exc);
+    }
+
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS endereco (
+                cliente_id int NOT NULL,
+                logradouro VARCHAR(30) NOT NULL,
+                cidade VARCHAR(30) NOT NULL,
+                estado VARCHAR(30) NOT NULL,
+                bairro VARCHAR(30),
+                numero INT,
+                PRIMARY KEY (cliente_id),
+                CONSTRAINT fk_cliente_id FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+            )
+        `)
+    } catch (exc) {
+        console.log("Create table endereco exception: ", exc);
+    }
 }
