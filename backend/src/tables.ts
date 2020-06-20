1bb9af4ccceca3350086d76b8ab6692f21a5e776
Igor S. Silvano <59940906+igorsvn@users.noreply.github.com>
2020-06-19 22:16:57 -0300

    tabelas;

--- backend/src/tables.ts
+++ backend/src/tables.ts
@@ -17,6 +17,7 @@ export const checkTables = async () => {
     try {
         await pool.query(`
             CREATE TABLE IF NOT EXISTS endereco (
+                id SERIAL PRIMARY KEY NOT NULL,
                 cliente_id int NOT NULL,
                 logradouro VARCHAR(30) NOT NULL,
                 cidade VARCHAR(30) NOT NULL,
@@ -25,6 +26,44 @@ export const checkTables = async () => {
                 numero INT,
                 PRIMARY KEY (cliente_id),
                 CONSTRAINT fk_cliente_id FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+            )
+        `)
+    } catch (exc) {
+        console.log("Create table endereco exception: ", exc);
+    }
+
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS endereco_carga (
+                id INT NOT NULL AUTO_INCREMENT,
+                logradouro VARCHAR(50),
+                numero INT,
+                bairro VARCHAR(50),
+                cidade VARCHAR(50),
+                estado VARCHAR(2)
+                PRIMARY KEY(id),
+            )
+        `)
+    } catch (exc) {
+        console.log("Create table endereco exception: ", exc);
+    }
+
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS carga (
+                id INT NOT NULL AUTO_INCREMENT,
+                id_endereco_carga INT NOT NULL,
+                id_cliente INT NOT NULL,
+                data_entrada VARCHAR(8),
+                data_entrega VARCHAR(8),
+                peso VARCHAR(50),
+                largura VARCHAR(50),
+                altura VARCHAR(50),
+                comprimento VARCHAR(50)
+                status VARCHAR(1)
+                PRIMARY KEY(id),
+                CONSTRAINT fk_endereco_carga FOREIGN KEY (id_endereco_carga) REFERENCES endereco_carga (id)
+                CONSTRAINT fk_cliente_carga FOREIGN KEY (id_cliente) REFERENCES cliente (id)
             )
         `)
     } catch (exc) {
